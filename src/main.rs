ec5cc44faae35d36e864a28e613dac1f3ff75a80
Alicja <alanotowska@yahoo.co.uk>
2024-09-11 14:29:03 +0200

    Adds simple message and a button

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
+#![allow(warnings)]
+
+use iced::widget;
+use iced::{Sandbox, Settings};
+
+// You can run every app you're developing in 2 ways:
+// - cargo run
+// - cargo watch -x run
+// (Obviously, there are more ways. :D But you don't need to worry about that now.)
+
 fn main() {
-    println!("Hello, world!");
+    Clicker::run(Settings::default());
 }
+
+struct Clicker {
+    // Application State
+    text: String,
+}
+impl Sandbox for Clicker {
+    type Message = u8;
+
+    fn new() -> Self {
+        println!("App created");
+        Self {
+            text: "Hello Alicja!".to_string(),
+        }
+    }
+
+    fn title(&self) -> String {
+        println!("Title set");
+        "Clicker on the rocks".to_string()
+    }
+
+    // Application View
+    // - how your app looks like
+    // - what's there? buttons, text fields, etc...
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        let text_widget = widget::text(&self.text);
+        let button_widget = widget::button("Say \"hi\" back").on_press(13);
+
+        println!("View rendered");
+        widget::row![text_widget, button_widget].into()
+
+        // You can find other widgets here: https://docs.rs/iced/latest/iced/widget/index.html
+        // Feel free to experiment. Just pay attention that you don't know how to send a Message
+        // that contains addition data, like integers, &strs etc... We'll get to that next time.
+
+        // Keep experimenting! ^-^
+        // let row1 = widget::row![widget::container(button_widget).padding(3)];
+        // let row2 = widget::row![widget::text("Alicja ma małego siusiaka")];
+        // widget::column![row1, row2, text_widget].into()
+    }
+
+    // Application Update
+    // - this is initated by interactions with View elements (widgets like buttons, sliders, etc...)
+    // - this can potentially change the application state
+    // - if the application state was changed the View will be updated accordingly
+    fn update(&mut self, message: Self::Message) {
+        println!("Message {} received by update method", message);
+        if message == 13 {
+            self.text = "Hello, Iced!".to_string();
+        }
+        println!("App state updated");
+    }
+
+    fn theme(&self) -> iced::Theme {
+        println!("Theme set");
+        iced::Theme::SolarizedDark
+    }
+}
+
+// General loop looks like this:  View <=> Update
+// more explicitly:
+// - if you interact with view -> the Message is sent
+// - when Message is sent -> update is executed
+// - update changes state -> view is executed to (potentialy) reflects these changes
